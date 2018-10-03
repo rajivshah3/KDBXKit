a00d50ecaa583efc5226f262bbd2a885480e1217
Rajiv Shah <rajivshah1@icloud.com>
2018-10-03 14:26:11 -0400

    Initial Commit

---
+++ KDBXKit/KDBXKit.h
@@ -0,0 +1,19 @@
+//
+//  KDBXKit.h
+//  KDBXKit
+//
+//  Created by Rajiv Shah on 10/3/18.
+//  Copyright © 2018 Rajiv Shah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KDBXKit.
+FOUNDATION_EXPORT double KDBXKitVersionNumber;
+
+//! Project version string for KDBXKit.
+FOUNDATION_EXPORT const unsigned char KDBXKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KDBXKit/PublicHeader.h>
+
+
